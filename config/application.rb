a9ae9e0d833c42c5655aa9145dcccb30b1049979
Elias Martinez <elias.martinez@amplemind.com>
2014-01-24 18:55:50 -0600

    bla bla

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Densigit
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.compile = true
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
